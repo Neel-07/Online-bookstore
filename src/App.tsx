fb08851ce75378bf2163faa862e3e5c68d00bfe3
Neel Mishra <neelmishra9125@gmail.com>
2025-02-06 22:21:53 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -215,7 +215,7 @@ function App() {
               </div>
             </div>
             <div className="border-t border-gray-700 mt-8 pt-4 text-center text-gray-400">
-              <p>&copy; 2024 BookSwap. All rights reserved.</p>
+              <p>&copy; 2025 BookSwap. All rights reserved.</p>
             </div>
           </footer>
         </motion.div>
